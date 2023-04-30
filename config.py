46ba25a610d8d666f4ae046b7ebe2dd166532296
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-04-30 19:42:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "21748181"))
+API_HASH = os.environ.get("API_HASH", "b1d962414e186e0778911f3183feac33")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6221359148:AAFm_TFyB_BG8MWGRxpVluB3f4qhi_iDX4U")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("5651594253") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Mongodb url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "mdisk")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Rishikesh001:Rishikesh001@cluster0.lqncnak.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5651594253")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(1255023013)
+ADMINS.append(5651594253)
 
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001949268590")) 
 
 # For Force Subscription
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @")
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "ziplinker_net")
 
 # true if forward should be avoided
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True")
